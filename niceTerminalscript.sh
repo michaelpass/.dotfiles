dd09fa6773cc22d3865b66e98e0fc7fbc3a9276f
Michael Pass <mik3pass@gmail.com>
2018-12-16 10:02:29 -0600

    Modified niceTerminalScript to have better documentation.

--- niceTerminalscript.sh
+++ niceTerminalscript.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Mac users - First install updated version of ZSH through Homebrew
+# brew install zsh
+
+# Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install custom plugins
+
+# Syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# Auto-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
- git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-:
+# Substring search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-searchl
