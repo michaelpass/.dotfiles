a767182c770c5f8a363df2c2f69867be364296e4
Michael Pass <mik3pass@gmail.com>
2021-11-15 15:06:39 -0600

    Removed Oh-My-Zsh installation

--- niceTerminalscript.sh
+++ niceTerminalscript.sh
@@ -4,7 +4,7 @@
 # brew install zsh
 
 # Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # ----------- Install custom plugins -----------
 
